87f8a70f1d867b57b94a234c46222978315353d2
MoonsBlacksmith <saulmarinho3@gmail.com>
2021-12-09 17:26:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,7 @@ cleaned_df = clean_df(df)
 
 ## get total abortions per period
 def get_total_abortions(df):
-    unique_periods = sorted(df.Period.unique())
-    ##new_values = df.loc[df['Period'] == 2000]
-    for i in range(len(unique_periods)):
-        total_sum = 0
-        period_values = df.loc[df['Period'] == unique_periods[i]]
-        print(period_values)
-        period_abortions = period_values['Induced_abortions'].tolist()
-        
-        for i in range(len(period_abortions)):
-            current_value = period_abortions[i]
-            total_sum += int(current_value)
-        ##print(f'Total abortions in {unique_periods[i]} is {total_sum}')
-
-       
-
-       
-     
-
-    
-
+    unique_periods = df[['Period', 'Induced_abortions']].groupby('Period').sum()
 get_total_abortions(cleaned_df)
 
 
